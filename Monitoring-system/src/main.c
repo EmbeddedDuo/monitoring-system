6ca1bbd4bbcbaff3157fe501e69ead18050fc747
janalo <janalo957@gmail.com>
2024-03-04 02:28:59 +0100

    spiff to store the picture

--- Monitoring-system/src/main.c
+++ Monitoring-system/src/main.c
@@ -34,9 +34,12 @@
 #include <nvs_flash.h>
 #include <sys/param.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+#include "esp_spiffs.h"
 
 // support IDF 5.x
 #ifndef portTICK_RATE_MS
@@ -50,8 +53,8 @@
 // WROVER-KIT PIN Map
 #ifdef BOARD_WROVER_KIT
 
-#define CAM_PIN_PWDN -1  //power down is not used
-#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_PWDN -1  // power down is not used
+#define CAM_PIN_RESET -1 // software reset will be performed
 #define CAM_PIN_XCLK 21
 #define CAM_PIN_SIOD 26
 #define CAM_PIN_SIOC 27
@@ -74,7 +77,7 @@
 #ifdef BOARD_ESP32CAM_AITHINKER
 
 #define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_RESET -1 // software reset will be performed
 #define CAM_PIN_XCLK 0
 #define CAM_PIN_SIOD 26
 #define CAM_PIN_SIOC 27
@@ -115,22 +118,22 @@ static camera_config_t camera_config = {
     .pin_href = CAM_PIN_HREF,
     .pin_pclk = CAM_PIN_PCLK,
 
-    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    // XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
     .xclk_freq_hz = 20000000,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
 
-    .pixel_format = PIXFORMAT_RGB565, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+    .pixel_format = PIXFORMAT_RGB565, // YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_QVGA,     // QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
 
-    .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
-    .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+    .jpeg_quality = 12, // 0-63, for OV series camera sensors, lower number means higher quality
+    .fb_count = 1,      // When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
 static esp_err_t init_camera(void)
 {
-    //initialize the camera
+    // initialize the camera
     esp_err_t err = esp_camera_init(&camera_config);
     if (err != ESP_OK)
     {
@@ -140,23 +143,106 @@ static esp_err_t init_camera(void)
 
     return ESP_OK;
 }
+
+
+esp_err_t mountSPIFFS(char * partition_label, char * base_path) {
+	ESP_LOGI(TAG, "Initializing SPIFFS file system");
+
+	esp_vfs_spiffs_conf_t conf = {
+		.base_path = base_path,
+		.partition_label = partition_label,
+		.max_files = 8,
+		.format_if_mount_failed = true
+	};
+
+	// Use settings defined above to initialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+	esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+	if (ret != ESP_OK) {
+		if (ret == ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount or format filesystem");
+		} else if (ret == ESP_ERR_NOT_FOUND) {
+			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+		}
+		return ret;
+	}
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(partition_label, &total, &used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+	}
+	ESP_LOGI(TAG, "Mount SPIFFS filesystem");
+	return ret;
+}
+
+static esp_err_t camera_capture(char *FileName, size_t *pictureSize)
+{
+    camera_fb_t *pic = esp_camera_fb_get();
+
+    // use pic->buf to access the image
+    // ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
+
+    if (!pic)
+    {
+        ESP_LOGE(TAG, "Camera Capture Failed");
+        return ESP_FAIL;
+    }
+
+   
+    FILE *f = fopen(FileName, "wb");
+    if (f == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        
+        return ESP_FAIL;
+    }
+    fwrite(pic->buf, pic->len, 1, f);
+    ESP_LOGI(TAG, "fb->len=%d", pic->len);
+    *pictureSize = (size_t)pic->len;
+    fclose(f);
+
+    esp_camera_fb_return(pic);
+	return ESP_OK;
+
+
+}
+
 #endif
 
-void app_main(void)
+void app_main()
 {
 #if ESP_CAMERA_SUPPORTED
-    if(ESP_OK != init_camera()) {
+    esp_err_t ret = nvs_flash_init();
+    ret = init_camera();
+    if (ESP_OK != ret)
+    {
         return;
     }
+
+    // Initialize SPIFFS
+	ESP_LOGI(TAG, "Initializing SPIFFS");
+	char *partition_label = "storage";
+	char *base_path = "/spiffs";
+	ret = mountSPIFFS(partition_label, base_path);
+	if (ret != ESP_OK) return;
+
+    size_t pictureSize;
+    char localFileName[64];
+    snprintf(localFileName, sizeof(localFileName)-1, "%s/picture.jpg", base_path);
 
     while (1)
     {
         ESP_LOGI(TAG, "Taking picture...");
-        camera_fb_t *pic = esp_camera_fb_get();
-
-        // use pic->buf to access the image
-        ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
-        esp_camera_fb_return(pic);
+        
+		ret = camera_capture(localFileName, &pictureSize);
+        ESP_LOGI(TAG, "camera_capture=%d",ret);
+        ESP_LOGI(TAG, "pictureSize=%d",pictureSize);
 
         vTaskDelay(5000 / portTICK_RATE_MS);
     }
